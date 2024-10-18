f4756c9ce2c7a34ef599d5f0502013f8bba96470
houssem_ben_jaafar <houssembenjaafar105@gmail.com>
2024-10-18 08:58:55 +0100

    fix little problem

--- src/app/search-filter.pipe.ts
+++ src/app/search-filter.pipe.ts
@@ -8,7 +8,7 @@ export class SearchFilterPipe implements PipeTransform {
   transform(list: any[], filterText: string): any {
     console.log("tronsfroming...")
     return list ? list.filter(item =>
-    item.modele.toLowerCase().includes(filterText)) : [];
+    item.modele.toLowerCase().includes(filterText.toLocaleLowerCase())) : [];
     }
 
 }
