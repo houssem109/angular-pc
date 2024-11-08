47718948fdb405448905c894eeefd4e9f4fd1505
houssem109 <houssembenjaafar105@gmail.com>
2024-11-08 09:33:05 +0100

    change the pc.guard

--- src/app/pc.guard.ts
+++ src/app/pc.guard.ts
@@ -1,12 +1,17 @@
-/* import { ActivatedRouteSnapshot,  CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
 import { AuthService } from './service/auth.service';
 
- */
+
+@Injectable({
+  providedIn: 'root'
+})
 
 
 
-
-
+/*
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from './service/auth.service';
@@ -24,20 +29,21 @@ export const pcGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: Rou
 };
 
 
-/*
-old version we cant use it and we can take the root and stat without constructor we take  it with inject directly
-export class pcGuard implements CanActivateFn {
-  constructor (private authService: AuthService,
-  private router : Router) {}
-  canActivate(
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot): boolean {
-  if (this.authService.isAdmin())
-  return true;
-  else
-  {
-  this.router.navigate(['app-forbidden']);
-  return false;
+
+old version we cant use it . we can take the root and auther without constructor we take  it with inject directly */
+export class pcGuard implements CanActivate {
+  
+  constructor(private authService: AuthService,
+    private router: Router) { }
+ 
+    canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    if (this.authService.isAdmin())
+      return true;
+    else {
+      this.router.navigate(['app-forbidden']);
+      return false;
+    }
   }
-}
-} */
+} 
